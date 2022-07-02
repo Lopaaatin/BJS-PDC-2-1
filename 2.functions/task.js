c49d304f97308d08eccb9e22bce7a761a50f4bae
Lopaaatin <img.russia@gmail.com>
2022-07-02 20:07:30 +0700

    Выполнено домашнее задание 2

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -2,30 +2,72 @@
 function getArrayParams(arr) {
   let min, max, sum, avg;
 
-  // Ваш код
+  min = arr[0];
+  for (let i=1; i < arr.length; i += 1) {
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+  }
+
+  max = arr[0];
+  for (let i=1; i < arr.length; i += 1) {
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+  }
+
+  sum = 0;
+  for (let i=0; i < arr.length; i += 1) {
+    sum = sum + arr[i];
+  }
+
+  avg = (Math.round((sum*100)/(arr.length)))/100;
 
   return { min: min, max: max, avg: avg };
 }
 
+
+
 // Задание 2
+
 function worker(arr) {
   let sum;
-
-  // Ваш код
-
+  sum = 0;
+  for (let i=0; i < arr.length; i += 1) {
+    sum = sum + arr[i];
+  }
   return sum;
 }
 
 function makeWork(arrOfArr, func) {
   let max;
-
-  // Ваш кода
-  // for ...
+  max = func(arrOfArr[0]);
+  for (let i=1; i < arrOfArr.length; i += 1) {
+    if (func(arrOfArr[i]) > max) {
+      max = func(arrOfArr[i]);
+    }
+  }
   
   return max;
 }
 
 // Задание 3
 function worker2(arr) {
-  // Ваш код
+  let max, min, distance;
+  
+  max = arr[0];
+  for (let i=1; i < arr.length; i += 1) {
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+  }
+
+  min = arr[0];
+  for (let i=1; i < arr.length; i += 1) {
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+  }
+
+  return distance = Math.abs(max - min);
 }
