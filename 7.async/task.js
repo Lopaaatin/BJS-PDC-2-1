f0214f69835a3b736abf26bed731a3fa531437ed
Lopaaatin <img.russia@gmail.com>
2022-08-05 23:10:56 +0700

    Вторая попытка сдать домашку, добавлен интервал, добавлено текущее время в будильники, укорочен getCurrentFormattedTime, укорочен removeClock

--- 7.async/task.js
+++ 7.async/task.js
@@ -19,32 +19,35 @@ class AlarmClock {
   }
 
   removeClock(id) {
-    let result;
     let removingAlarmIndex = this.alarmCollection.findIndex((item) => item.id == id);
     if (removingAlarmIndex >= 0) {
       this.alarmCollection.splice(removingAlarmIndex, 1);
-      result = true;
-    } else {
-      result = false;
-    }
-    return result;
+      return true;
+    } 
+    return false;
   }
 
   getCurrentFormattedTime() {
     let date = new Date;
     let hours = date.getHours();
     let minutes = date.getMinutes();
-    let result;
-    if ((hours < 10) && (minutes < 10)) {
-      result = (`0${hours}:0${minutes}`)
-    } else if (hours < 10) {
-      result = (`0${hours}:${minutes}`)
-    } else if (minutes < 10) {
-      result = (`${hours}:0${minutes}`)
-    } else {
-      result = (`${hours}:${minutes}`)
+    // let result;
+    // if ((hours < 10) && (minutes < 10)) {
+    //   result = (`0${hours}:0${minutes}`)
+    // } else if (hours < 10) {
+    //   result = (`0${hours}:${minutes}`)
+    // } else if (minutes < 10) {
+    //   result = (`${hours}:0${minutes}`)
+    // } else {
+    //   result = (`${hours}:${minutes}`)
+    // }
+    if (hours < 10) {
+      hours = (`0 + ${hours}`)
     }
-    return result;
+    if (minutes < 10) {
+      minutes = (`0 + ${minutes}`)
+    }
+    return (`${hours}:${minutes}`);
   }
 
   start() {
@@ -56,7 +59,7 @@ class AlarmClock {
     }
 
     if (this.timerId == null) {
-      this.timerId = setInterval(this.alarmCollection.forEach(checkClock),);
+      this.timerId = setInterval(this.alarmCollection.forEach(checkClock), 60000);
     }
   }
 
@@ -81,18 +84,19 @@ class AlarmClock {
 }
 
 let phoneAlarm = new AlarmClock;
+let date = new Date;
 
-phoneAlarm.addClock("09:00", () => console.log('Подъём'), 1);
-phoneAlarm.addClock("09:01", () => console.log('Подъём же!'), 2);
+phoneAlarm.addClock([`${date.getHours()}:${date.getMinutes()}`], () => console.log('Подъём'), 1);
+phoneAlarm.addClock(`${date.getHours()}:${date.getMinutes()+1}`, () => console.log('Подъём же!'), 2);
 // phoneAlarm.addClock("09:00", () => console.log('Почти проспал'));
 
-phoneAlarm.addClock("09:00", () => {
+phoneAlarm.addClock(`${date.getHours()}:${date.getMinutes()+2}`, () => {
   console.log('Подъём давай уже');
   phoneAlarm.clearAlarms();
   phoneAlarm.printAlarms();  
   }, 3);
 
-  phoneAlarm.addClock("09:05", () => console.log('Подъём блин'), 1);
+  phoneAlarm.addClock(`${date.getHours()}:${date.getMinutes()+5}`, () => console.log('Подъём блин'), 1);
 
   phoneAlarm.printAlarms();
 
